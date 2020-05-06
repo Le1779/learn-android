9ae137c6a4f18794de3ec901039cc1cb7b5b5beb
kevinle <le8419951779@gmail.com>
2020-05-06 14:30:40 +0800

    init

---
+++ app/src/main/java/kevin/le/learnandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package kevin.le.learnandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
